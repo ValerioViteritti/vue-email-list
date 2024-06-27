196505d88a6bfa4019c946593a9065a69b55e858
ValerioViteritti <vviteritti@gmail.com>
2024-06-27 15:13:24 +0200

    1.0.0 finito con bonus

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
 
-
-
 const { createApp } = Vue;
 
 createApp({
@@ -13,33 +11,27 @@ createApp({
         return {
             // array che conterrà le mail generate
             arrayEmail: [],
+            // Numero fisso di email.da generare
             numberEmail: 10
         }
-
     },
     methods: {
-        
-
-
+        genera(numberEmail) {
+            // Svuota l'array prima di generarne di nuove
+            this.arrayEmail = [];
+            // ciclo
+            for (let i = 0; i < numberEmail; i++){
+                // apposita API di Boolean
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((result) => {
+                    // push delle email che verranno generate dal ciclo
+                    this.arrayEmail.push(result.data.response);
+                });
+            }
+        }
+    },
+    mounted(){
+        // Genera l'email quando il numero prestabilito è raggiunto
+        this.genera(this.numberEmail);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }).mount('#app');
